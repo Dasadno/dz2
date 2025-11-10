6dfca1e7978e8e34ac44fed397e251ea91ef94c1
Dasadno <Yakaleks96@gmail.com>
2025-11-10 09:58:54 +0500

    Добавьте файлы проекта.

---
+++ 2/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartProgress(object sender, RoutedEventArgs e)
+        {
+            var thread1 = new Thread(() =>
+            {
+                thread1.Priority = ThreadPriority.Highest;
+                for (int i = 0; i < 100; i++)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        
+                    });
+                }
+            });
+        }
+    }
+}
---
+++ dz2/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Windows;
+
+namespace dz2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void ProgressBad(object sender, RoutedEventArgs e)
+        {
+            var thread = new Thread(() =>
+            {
+
+                for (int i = 0; i < 100; i+=10) {
+                    Thread.Sleep(500);
+                    Dispatcher.Invoke(() =>
+                    {
+                        Progres1.Value = i;
+                    });
+                }
+
+            });
+
+        }
+        private void HighestThread(object sender, RoutedEventArgs e)
+        {
+            var thread = new Thread(() =>
+            {
+                for (int i = 100; i > 0; i -= 10)
+                {
+                    Thread.Sleep(500);
+                    Dispatcher.Invoke(() =>
+                    {
+                        Progres1.Value = i;
+                    });
+                }
+
+            });
+
+            thread.Priority = ThreadPriority.Highest;
+
+            if(thread.Priority == ThreadPriority.Highest)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    OutputHighestThread.Text = $"Поток уровня {thread.Priority}";
+                });
+            }
+
+            thread.Start();
+        }
+
+        private void UpdateUI(object sender, RoutedEventArgs e)
+        {
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    TextOut.Text = "text";
+                }
+                catch (InvalidOperationException err)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        ExOut.Text = err.Message;
+                    });
+                }
+                catch (Exception err)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        ExOut.Text = "Unknown Exception";
+                    });
+                }
+            });
+            thread.Start();
+        }
+    }
+}
